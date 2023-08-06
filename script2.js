e97322cd6ef9757f2294498d2077b9801c8fdabc
NiikNiik <nikoajani7@gmail.com>
2023-08-05 21:42:52 -0400

    remove api key

--- script2.js
+++ script2.js
@@ -5,7 +5,7 @@ const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null; // Variable to store user's message
-const API_KEY = "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" // Paste your API key here
+const API_KEY = "Place an API key" //Paste your API key here
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
